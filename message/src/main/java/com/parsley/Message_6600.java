e65ee1571793810cc5debcaa64e11de8f71b8225
Parsley0 <1528592734@qq.com>
2023-01-11 18:26:25 +0800

    message_service

---
+++ message/src/main/java/com/parsley/Message_6600.java
@@ -0,0 +1,18 @@
+package com.parsley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: Parsley
+ * @Date: 2023/01/11/18:22
+ * @Description:
+ */
+@SpringBootApplication
+public class Message_6600 {
+    public static void main(String[] args) {
+        SpringApplication.run(Message_6600.class,args);
+    }
+}
